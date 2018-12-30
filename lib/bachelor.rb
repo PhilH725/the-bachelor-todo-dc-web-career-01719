a7fea7cde90c97dd8bdb65674a41456d40a87dfa
Phil Hartley <philh725@gmail.com>
2018-12-30 20:21:18 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -36,7 +36,15 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_number, contestants_array|
+    contestants_array.each do |contestant_hash|
+      contestant_hash.each do |key, value|
+        if value == hometown
+          return contestant_hash["occupation"]
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
